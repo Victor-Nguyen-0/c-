ab055bbd4cc725494cc7c8f80452ce20d6be0e3b
Victor Nguyen <vln3dd@virginia.edu>
2024-08-14 23:37:00 -0400

    basic operations

--- hello_world/Program.cs
+++ hello_world/Program.cs
@@ -46,32 +46,70 @@
 // Console.WriteLine("Have " + intOne + " on us. :)");
 // Console.WriteLine("~~~~~Pretend this is a free coupon~~~~~ yay");
 
-// Formatting literal strings
-Console.WriteLine("---------- Literal String Formatting ----------");
+// // Formatting literal strings
+// Console.WriteLine("---------- Literal String Formatting ----------");
 
-Console.WriteLine("Hello\nWorld!"); // \n will add (N)ew line
-Console.WriteLine("Hello\tWorld!"); // \t will add (t)ab
+// Console.WriteLine("Hello\nWorld!"); // \n will add (N)ew line
+// Console.WriteLine("Hello\tWorld!"); // \t will add (t)ab
 
-Console.WriteLine("Hello \"World\"!"); // for quotes in strings -- use \" \" sequence
+// Console.WriteLine("Hello \"World\"!"); // for quotes in strings -- use \" \" sequence
 
-Console.WriteLine("c:\\file\\path"); // double \\ for file path using single \
+// Console.WriteLine("c:\\file\\path"); // double \\ for file path using single \
 
-//Verbatim string literal -- keeps all whitespace and characters, use @ before literal string
-Console.WriteLine(@" space     test   
-    how many spaces         will this look like");
+// //Verbatim string literal -- keeps all whitespace and characters, use @ before literal string
+// Console.WriteLine(@" space     test   
+//     how many spaces         will this look like");
 
-// Unicode escape characters
-// Kon'nichiwa World
-Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!"); // \uXXXX 4 number sequence -- doesnt work on my machine :) 
+// // Unicode escape characters
+// // Kon'nichiwa World
+// Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!"); // \uXXXX 4 number sequence -- doesnt work on my machine :) 
 
-// String concatenation
-string firstName = "Bob";
-string greeting = "Hello";
-Console.WriteLine(greeting + " " + firstName + "!");
+// // String concatenation
+// string firstName2 = "Bob";
+// string greeting = "Hello";
+// Console.WriteLine(greeting + " " + firstName2 + "!");
 
-// String interpolation
-Console.WriteLine($"Hello {firstName}!"); // less lines, using $ to bring up interpolation template
-//can do multiple variables and strings
-int version = 11;
-string updateText = "Update to Windows";
-Console.WriteLine($"{updateText} {version}!");
+// // String interpolation
+// Console.WriteLine($"Hello {firstName}!"); // less lines, using $ to bring up interpolation template
+// //can do multiple variables and strings
+// int version = 11;
+// string updateText = "Update to Windows";
+// Console.WriteLine($"{updateText} {version}!");
+
+// Basic Operations
+Console.WriteLine("---------- Basic Operations ----------");
+
+// Implicit Conversion -- compiler will implciitly convert variables into strings when concatenating if able
+string firstName3 = "Bob";
+int daysWorked = 7;
+Console.WriteLine(firstName3 + " has worked " + daysWorked + 9 + " days."); // here it assumes everything is a string so the int variable and number won't add together
+Console.WriteLine(firstName3 + " has worked " + (daysWorked + 9) + " days."); // here the parenthesis will form an operator and indicate an actual operation to be performed
+// The + operator here is overloaded. Basically a polymorphism that lets it concatenate strings or add integers in an operation
+
+// Casting -- type of data conversion that instructs compiler to temporarily treat a value as if it were a different data type
+int first = 7;
+int second = 5;
+decimal quotient = (decimal)first / second; // second cast for second variable is redundant 
+Console.WriteLine(quotient);
+
+// % operator for remainders after division
+Console.WriteLine($"Remainder should be 1: {3%2}");
+
+// Order of Operations -- PEMDAS -- also pay attention to ints vs decimal/floats
+int value1 = 3 + 4 * 5;
+int value2 = (3 + 4) * 5;
+Console.WriteLine(value1);
+Console.WriteLine(value2);
+
+// Increment/Decrement is same as other languages --> ++ or -- or += or -= 
+int value3 = 1;
+value3++;
+// Increment/Decrement can be placed before or after variable with differing effects
+Console.WriteLine("First: " + value3); // Shows 2 -- current value
+Console.WriteLine($"Second: {value3++}"); // Returns 2 first, then increments, should 3 now but not outputted
+Console.WriteLine("Third: " + value3); // Returns the 3
+Console.WriteLine("Fourth: " + (++value3)); // Increments first, then returns 4
+
+// Operations that output ints will use the whole integer and cut off the rest
+Console.WriteLine(1/2); //  should be 0.5, but int will cut off and it will show 0
+Console.WriteLine(1.0/2); // 1.0 designates it as a double now, so it should output 0.5 //// can also use decimal m and float f
--- hello_world/obj/Debug/net8.0/hello_world.AssemblyInfo.cs
+++ hello_world/obj/Debug/net8.0/hello_world.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a83ee7d7fd3dfc65cc8598c1b775a8cd6ab65c0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbba163fb6d1758cfc276e1694d3963b7a07d32c")]
 [assembly: System.Reflection.AssemblyProductAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
