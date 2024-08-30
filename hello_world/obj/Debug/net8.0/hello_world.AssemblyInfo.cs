98c6e141f5b868dd6d84dee2b37b7033acae61ad
Victor Nguyen <vln3dd@virginia.edu>
2024-08-29 20:29:55 -0400

    replaced if with switch and added do while for menu selection

--- hello_world/obj/Debug/net8.0/hello_world.AssemblyInfo.cs
+++ hello_world/obj/Debug/net8.0/hello_world.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76905275fa1262db6a738ff4cd5ac20c1cf7a23e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c21ecb2e3d47f556668191946f49bd2e59e653c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
