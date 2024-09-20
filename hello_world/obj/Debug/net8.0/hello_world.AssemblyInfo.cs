e1d732e86552bc7e2a81772f08de632f9ac23e1c
Victor Nguyen <vln3dd@virginia.edu>
2024-09-19 23:02:53 -0400

    misc

--- hello_world/obj/Debug/net8.0/hello_world.AssemblyInfo.cs
+++ hello_world/obj/Debug/net8.0/hello_world.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7146f5ad78bbb10fb3a41a5c80553d8357602a9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b31192b0eff4f4e2c193fa7b8aa82625a2e794ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
