657af39c50ae206c8625fe6acb257f7a7c0240ce
Victor Nguyen <vln3dd@virginia.edu>
2024-10-03 21:20:32 -0400

    misc

--- do_while/obj/Debug/net8.0/do_while.AssemblyInfo.cs
+++ do_while/obj/Debug/net8.0/do_while.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("do_while")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c65102178a8b3f1bd7cd220654c54b107c1789a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0357b5613db6716928d823df7684bbbf5bf7d02d")]
 [assembly: System.Reflection.AssemblyProductAttribute("do_while")]
 [assembly: System.Reflection.AssemblyTitleAttribute("do_while")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- hello_world/obj/Debug/net8.0/hello_world.AssemblyInfo.cs
+++ hello_world/obj/Debug/net8.0/hello_world.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c65102178a8b3f1bd7cd220654c54b107c1789a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0357b5613db6716928d823df7684bbbf5bf7d02d")]
 [assembly: System.Reflection.AssemblyProductAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
